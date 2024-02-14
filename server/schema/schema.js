1b286147b14d6335735009aa2dc9224fc269435e
jamesryan-dev <hello@jamesryan.co>
2024-02-14 21:10:17 +0000

    server w/ db connected and root queries updated

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,81 @@
-const { GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql')
-const { projects, clients } = require('../sampleData')
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require("graphql");
+
+const Project = require("../models/Project");
+const Client = require("../models/Client");
 
 const ClientType = new GraphQLObjectType({
-    name: 'Client',
-    fields: () => ({
-        id: { type: GraphQLID}, 
-        name: { type: GraphQLString },
-        email: { type: GraphQLString },
-        phone: { type: GraphQLString },
-    })
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    clientId: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
+
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        // return clients.find((client) => client.id === parent.clientId);
+        return Client.findById(parent.clientId);
+      },
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    projects: {
+      type: GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return Project.find({});
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // TO DO:
+        // add mongoose function to get data from database
+        // return projects.find((client) => client.id === args.id);
+        return Project.findById(args.id);
+      },
+    },
+    clients: {
+      type: GraphQLList(ClientType),
+      resolve(parent, args) {
+        // return clients;
+        return Client.find({});
+      },
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // TO DO:
+        // add mongoose function to get data from database
+        // return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
