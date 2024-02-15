d9e3924d6f17d2936fd77835a1551ed139a93534
jamesryan-dev <hello@jamesryan.co>
2024-02-15 22:44:31 +0000

    client - init project w/ bootstrap and header component

---
+++ client/components/Header.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import logo from "./assets/logo.png";
+
+export default function Header() {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <div className="d-flex">
+            <Image
+              src={logo}
+              alt="logo"
+              width={30}
+              height={30}
+              className="mr-2"
+            />
+            <div>ProjectMgmt</div>
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+}
