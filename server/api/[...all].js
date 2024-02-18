7d7827348302bea05e289a708b736ee0a80b66a0
jamesryan-dev <hello@jamesryan.co>
2024-02-18 11:57:36 +0000

    server - vercel serverless

---
+++ server/api/[...all].js
@@ -0,0 +1,28 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("../schema/schema"); // Adjust the path as necessary
+const connectDB = require("../config/db"); // Adjust the path as necessary
+
+const app = express();
+
+// Connect to the database
+connectDB();
+
+app.use(cors());
+
+// Setup GraphQL endpoint
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  }),
+);
+
+// Export the serverless handler
+module.exports = app;
+module.exports.handler = serverless(app);
